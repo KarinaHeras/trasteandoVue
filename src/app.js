7f44f65b9167f87ecdd198cfee4de1c4313c68a1
KarinaHeras <karinaheras1847@gmail.com>
2019-03-07 18:12:07 +0100

    Trasteando con Vue, agregando lo recientemente  aprendido en llamadas con async await

---
+++ src/app.js
@@ -0,0 +1,49 @@
+Vue.component('user-item',
+            {
+            props: ['user'],
+            template: `<li>{{ user.name }} lives in {{ user.address.city }}</li>`
+        })
+
+
+        Vue.component('user-list', {
+            props:['users'],
+            template: `
+            <ul>
+              <user-item
+              v-for="(user, index) in users"
+              :user="user">
+              </user-item>
+            </ul>
+            `
+        })
+
+    const endpoint = 'https://jsonplaceholder.typicode.com/users'
+    
+    function getUsers(url){
+        const users = fetch(url).then(function(response){
+            return response.json()
+        })
+        return users    
+    }
+
+    async function callAndPaint(){
+       const myUsers = await getUsers(endpoint)
+       console.log(myUsers)
+        const app = new Vue({
+            el: '#app',
+            data: {
+              users: myUsers
+            },
+            template: `
+               <user-list :users="users"> </user-list>
+               `
+        })
+    }
+    callAndPaint()
+    
+    //[{ name: 'User 1'} , {name: 'User 2' }]// getUsers(endpoint)
+
+    
+
+    
+
