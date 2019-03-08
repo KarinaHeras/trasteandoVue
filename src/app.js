9563be48ab6eca5784eb652afdcc8622c0c62172
KarinaHeras <karinaheras1847@gmail.com>
2019-03-08 11:35:41 +0100

    colocando un poco de color

--- src/app.js
+++ src/app.js
@@ -1,47 +1,47 @@
 Vue.component('user-item',
             {
             props: ['user'],
-            template: `<li>{{ user.name }} lives in {{ user.address.city }}</li>`
+            template: `<li class="nameList">{{ user.name }} lives in {{ user.address.city }}</li>`
         })
 
 
-        Vue.component('user-list', {
-            props:['users'],
-            template: `
-            <ul>
-              <user-item
-              v-for="(user, index) in users"
-              :user="user">
-              </user-item>
-            </ul>
+Vue.component('user-list', {
+    props:['users'],
+    template: `
+    <ul>
+        <user-item
+        v-for="(user, index) in users"
+        :user="user">
+        </user-item>
+    </ul>
+    `
+})
+
+const endpoint = 'https://jsonplaceholder.typicode.com/users'
+
+function getUsers(url){
+    const users = fetch(url).then(function(response){
+        return response.json()
+    })
+    return users    
+}
+
+async function callAndPaint(){
+    const myUsers = await getUsers(endpoint)
+    console.log(myUsers)
+    const app = new Vue({
+        el: '#app',
+        data: {
+            users: myUsers
+        },
+        template: `
+            <user-list :users="users"> </user-list>
             `
-        })
+    })
+}
+callAndPaint()
 
-    const endpoint = 'https://jsonplaceholder.typicode.com/users'
-    
-    function getUsers(url){
-        const users = fetch(url).then(function(response){
-            return response.json()
-        })
-        return users    
-    }
-
-    async function callAndPaint(){
-       const myUsers = await getUsers(endpoint)
-       console.log(myUsers)
-        const app = new Vue({
-            el: '#app',
-            data: {
-              users: myUsers
-            },
-            template: `
-               <user-list :users="users"> </user-list>
-               `
-        })
-    }
-    callAndPaint()
-    
-    //[{ name: 'User 1'} , {name: 'User 2' }]// getUsers(endpoint)
+//[{ name: 'User 1'} , {name: 'User 2' }]// getUsers(endpoint)
 
     
 
